be8aeda552bb5c1ca7a05426ff3e6a0d3a495807
mario5445 <91668245+mario5445@users.noreply.github.com>
2024-04-21 21:23:30 +0200

    Add main to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <title>Scheidt&bachmann</title>
     <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
     <script>
@@ -14,10 +17,12 @@
     <style>
         :root {
             --primary-color: #1f2833;
-            --secondary-color: #b69b04;
+            --secondary-color: #cfcfca;
+            --terciary-color: #aed3ffee;
+            --light-gray-color: #9c9a9a;
 
             --font-color-gray: #423f3f; 
-            --font-color-blue: #66fcf1;
+            --font-color-darkblue: #1f2833;
             --font-color-white: #eee;
         }
         * {
@@ -37,7 +42,7 @@
         }
 
         body {
-            font-family: 'Open Sans', sans-serif; 
+            font-family: 'Montserrat', sans-serif; 
             font-weight: 400;
             line-height: 1.6;
         }
@@ -45,6 +50,10 @@
         a {
             color: inherit;
             text-decoration: none;
+        }
+
+        header {
+            margin-bottom: 3rem;
         }
 
         .nav {
@@ -59,6 +68,10 @@
             color: var(--font-color-white);
         }
 
+        .nav-logo {
+            height: 5rem;
+        }
+
         .nav-logo--img {
             height: 5rem;
         }
@@ -71,6 +84,77 @@
             /* margin-right: 10rem; */
         }
 
+        main {
+            height: calc(100vh - 5rem - 2rem - 2rem - 3rem);
+            padding-right: 3rem;
+            padding-left: 3rem;
+        }
+
+        .heading-main {
+            font-size: 4rem;
+            letter-spacing: 0.45rem;
+            margin-bottom: 1.5rem;
+            font-weight: 800;
+        }
+
+        .heading-secondary {
+            font-size: 3.2rem;
+            letter-spacing: 0.5rem;
+            margin-bottom: 1.5rem;
+            font-weight: 300;
+        }
+
+        .main-content{
+            display: flex;
+            justify-content: space-between;
+            gap: 10rem;
+        }
+
+        .main-content-left {
+            flex-basis: 60%;
+        }
+
+        .main-content-right {
+            flex-grow: 1;
+        }
+
+        .overview {
+            height: 60rem;
+            border-radius: 3px;
+            padding: 3rem;
+            box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        }
+
+        .parkingSlotsOverview {
+            background-color: var(--terciary-color);
+        }
+
+        .parkedCarsOverview {
+            background-color: var(--secondary-color);
+        }
+
+        .overviewItem {
+            font-size: 4rem;
+            color: var(--font-color-gray);
+            border-bottom: 1px solid var(--light-gray-color);
+        }
+
+        .parkingSlot {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .overviewItem:not(:last-child) {
+            margin-bottom: 2.5rem;
+        }
+
+        .occupied {
+            color: #ce6139;
+        }
+
+        .free {
+            color: #178b17;
+        }
     </style>
 </head>
 <body>
@@ -83,7 +167,7 @@
                 <a class="nav-list--item" href="#">Dashboard</a>
                 <a class="nav-list--item" href="#">Allowed Cars</a>
                 <a class="nav-list--item" href="#">Modes</a>
-                <a class="nav-list--item" href="#">Users</a>
+                <a class="nav-list--item" href="#">Profile</a>
                 <div class="nav-logout-item">
                     <a href="#">Logout</a>
                 </div>
@@ -91,12 +175,29 @@
         </nav>
     </header>
     <main>
-        <div class="parkovisko" style="display: flex; justify-content: space-around;">
-            @foreach ($slots as $slot)
-                <div id="{{$slot->sensor->special_id}}">
-                    <p>{{$slot->occupied == 1 ? "obsadené" : "voľné"}}</p>
+        <h1 class="heading-main">Parking house {{ucfirst($parkingHouse->name)}} in {{ucfirst($parkingHouse->location)}}</h1>
+        <div class="main-content">
+            <div class="main-content-left">
+                <h2 class="heading-secondary">{{$slots->where('occupied', 0)->count()}}/{{$slots->count()}} free</h2>
+                <div class="overview parkingSlotsOverview" @style(['overflow-y: scroll' => $slots->count() > 6])>
+                    @foreach ($slots as $parkingSlot)
+                        <div class="parkingSlot overviewItem" id="{{$parkingSlot->sensor->special_id}}">
+                            <div class="parkingSlot--name">{{$parkingSlot->sensor->name}}</div>
+                            <div @class(['parkingSlot--occupied', 'occupied' => $parkingSlot->occupied, 'free' => ! $parkingSlot->occupied]) class="">{{$parkingSlot->occupied == 1 ? "Occupied" : "Free"}}</div>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
+            <div class="main-content-right">
+                <h2 class="heading-secondary">Mode: everyone</h2>
+                <div class="overview parkedCarsOverview" @style(['overflow-y: scroll' => $parkedCars->count() > 6])>
+                    @foreach ($parkedCars as $car)
+                        <div class="parked-car overviewItem" id="{{$car->spz}}">
+                            <div class="parked-car--id">{{$car->spz}}</div>
+                        </div>
+                    @endforeach
+                </div>   
+            </div>
         </div>
     </main>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Models\ParkedCar;
+use App\Models\ParkingHouse;
 use App\Models\ParkingSlot;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index', ["slots" => ParkingSlot::with('sensor')->get()]);
+    return view('index', ["slots" => ParkingSlot::with('sensor')->get(), "parkingHouse" => ParkingHouse::where('id', 1)->first(), "parkedCars" => ParkedCar::all()]);
 });
 
 Route::get('/banasko', function(){
