072f9b81b572a70ae8dd341369252cc6795a1d52
mario5445 <91668245+mario5445@users.noreply.github.com>
2024-04-23 00:02:19 +0200

    no ty vole

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,154 +8,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <title>Scheidt&bachmann</title>
     <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
     <script>
         const banaskoRoute = "{{ route('banasko') }}";
     </script>
-    <script src="{{ asset('js/mqtt.js') }}""></script>
-    <style>
-        :root {
-            --primary-color: #1f2833;
-            --secondary-color: #cfcfca;
-            --terciary-color: #aed3ffee;
-            --light-gray-color: #9c9a9a;
-
-            --font-color-gray: #423f3f; 
-            --font-color-darkblue: #1f2833;
-            --font-color-white: #eee;
-        }
-        * {
-            margin: 0;  
-            padding: 0;
-        }  
-
-        *,
-        *::after,
-        *::before {
-            box-sizing: inherit;
-        }
-
-        html {
-            box-sizing: border-box;
-            font-size: 62.5%; // 1rem == 10px
-        }
-
-        body {
-            font-family: 'Montserrat', sans-serif; 
-            font-weight: 400;
-            line-height: 1.6;
-        }
-        
-        a {
-            color: inherit;
-            text-decoration: none;
-        }
-
-        header {
-            margin-bottom: 3rem;
-        }
-
-        .nav {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 2rem;
-            padding-left: 3rem;
-            padding-right: 3rem;
-            font-size: 1.9rem;
-            background-color: var(--primary-color);
-            color: var(--font-color-white);
-        }
-
-        .nav-logo {
-            height: 5rem;
-        }
-
-        .nav-logo--img {
-            height: 5rem;
-        }
-
-        .nav-list {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            gap: 6rem;
-            /* margin-right: 10rem; */
-        }
-
-        main {
-            height: calc(100vh - 5rem - 2rem - 2rem - 3rem);
-            padding-right: 3rem;
-            padding-left: 3rem;
-        }
-
-        .heading-main {
-            font-size: 4rem;
-            letter-spacing: 0.45rem;
-            margin-bottom: 1.5rem;
-            font-weight: 800;
-        }
-
-        .heading-secondary {
-            font-size: 3.2rem;
-            letter-spacing: 0.5rem;
-            margin-bottom: 1.5rem;
-            font-weight: 300;
-        }
-
-        .main-content{
-            display: flex;
-            justify-content: space-between;
-            gap: 10rem;
-        }
-
-        .main-content-left {
-            flex-basis: 60%;
-        }
-
-        .main-content-right {
-            flex-grow: 1;
-        }
-
-        .overview {
-            height: 60rem;
-            border-radius: 3px;
-            padding: 3rem;
-            box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-        }
-
-        .parkingSlotsOverview {
-            background-color: var(--terciary-color);
-        }
-
-        .parkedCarsOverview {
-            background-color: var(--secondary-color);
-        }
-
-        .overviewItem {
-            font-size: 4rem;
-            color: var(--font-color-gray);
-            border-bottom: 1px solid var(--light-gray-color);
-        }
-
-        .parkingSlot {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .overviewItem:not(:last-child) {
-            margin-bottom: 2.5rem;
-        }
-
-        .occupied {
-            color: #ce6139;
-        }
-
-        .free {
-            color: #178b17;
-        }
-    </style>
+    <script src="{{ asset('js/mqtt.js') }}"></script>
 </head>
 <body>
     <header>
@@ -163,14 +24,19 @@
             <div class="nav-logo">
                 <img class="nav-logo--img" src="{{ asset('img/logo_white.png') }}" alt="Logo">
             </div> 
+            <div class="mobile-nav">
+                <div class="mobile-nav-container">
+                    <div class="mobile-nav-btn mobile-nav-btn--first"></div>
+                    <div class="mobile-nav-btn mobile-nav-btn--middle"></div>
+                    <div class="mobile-nav-btn mobile-nav-btn--last"></div>
+                </div>
+            </div>
             <div class="nav-list">
                 <a class="nav-list--item" href="#">Dashboard</a>
                 <a class="nav-list--item" href="{{ route('allowedCars') }}">Allowed Cars</a>
                 <a class="nav-list--item" href="#">Modes</a>
                 <a class="nav-list--item" href="#">Profile</a>
-                <div class="nav-logout-item">
-                    <a href="#">Logout</a>
-                </div>
+                <a class="nav-list--item" href="#">Logout</a>
             </div>
         </nav>
     </header>
@@ -178,7 +44,7 @@
         <h1 class="heading-main">Parking house {{ucfirst($parkingHouse->name)}} in {{ucfirst($parkingHouse->location)}}</h1>
         <div class="main-content">
             <div class="main-content-left">
-                <h2 class="heading-secondary">{{$slots->where('occupied', 0)->count()}}/{{$slots->count()}} free</h2>
+                <h3 class="heading-tertiary">{{$slots->where('occupied', 0)->count()}}/{{$slots->count()}} free</h3>
                 <div class="overview parkingSlotsOverview" @style(['overflow-y: scroll' => $slots->count() > 6])>
                     @foreach ($slots as $parkingSlot)
                         <div class="parkingSlot overviewItem" id="{{$parkingSlot->sensor->special_id}}">
@@ -189,7 +55,7 @@
                 </div>
             </div>
             <div class="main-content-right">
-                <h2 class="heading-secondary">Mode: everyone</h2>
+                <h2 class="heading-tertiary">Mode: everyone</h2>
                 <div class="overview parkedCarsOverview" @style(['overflow-y: scroll' => $parkedCars->count() > 6])>
                     @foreach ($parkedCars as $car)
                         <div class="parked-car overviewItem" id="{{$car->spz}}">
@@ -200,5 +66,11 @@
             </div>
         </div>
     </main>
+    <footer>
+        <div>
+            2024&copy; Made by Skibidi Marek and Mario
+        </div>
+    </footer>
+    <script src="{{asset('js/index.js')}}"></script>
 </body>
 </html>
